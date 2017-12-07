802f8791fa8b56a5a12c0ea3f7c2ec4034738c8d
NextDorm@gmail.com <NextDorm@gmail.com>
2017-12-07 00:29:05 -0500

    Began updating addpost form

--- views/addpost/index.php
+++ views/addpost/index.php
@@ -12,17 +12,16 @@
   <?php }?>
   <div class="row">
       <div class="span8">
-        <form action="<?php echo BASE_URL?>addpost/<?php if(isset($task)) echo $task; ?>" method="post" onsubmit="editor.post()">
+        <form action="<?php echo BASE_URL?>addpost/<?php if(isset($task)) echo $task; ?>" method="post" id="post-form">
           <label>Title</label>
-          <input type="text" class="span6" name="post_title" value="<?php if(isset($title)) echo $title?>">
+          <input id="title" type="text" class="span6" name="post_title" value="<?php if(isset($title)) echo $title?>">
           <input type="hidden" name="date" value="<?php echo date("Y-m-d H:i:s"); ?>"/>
 		  <label>Category</label>
-          <select class="span6" name="categoryID"><option disabled value>-- Select Category --</option>
+          <select id="category" class="span6" name="categoryID"><option selected disabled value>-- Select Category --</option>
 			<?php
 				$list = new Category();
 				$i = 1;
 				$categories = $list->getCategories();
-				?><script>alert(<?php var_dump($categories); ?>);</script><?php
 				foreach($categories as $category) {
 					$selected = '';
 					if($category["categoryID"] === $categoryID) { $selected = ' selected'; }
@@ -30,13 +29,77 @@
 				}
 			?>
 		  </select>
-     			<label>Content</label>
-          <textarea id="tinyeditor" name="post_content" style="width:556px;height: 200px"><?php if(isset($content)) echo $content?></textarea>
+     			<label id="content-label">Content</label>
+          <textarea id="post-content" name="post_content" style="width:556px;height: 200px"><?php if(isset($content)) echo $content?></textarea>
     			<br/>
           <input type="hidden" name="pID" value="<?php if(isset($pID)) { echo $pID; } ?>"/>
 		  <input type="hidden" name="uID" value="<?php echo $user->uID; ?>"/>
-          <button id="submit" type="submit" class="btn btn-primary" >Submit</button>
-        </form>        
+          <button id="add-submit" class="btn btn-primary" >Submit</button>
+        </form>
+		<script>
+			$(document).ready(function(){
+				$('#title').on('change', function() {
+					console.log($(this).attr('value'));
+					if($(this).attr('value') == '') {
+						$(this).css('border','1px solid #F00');
+					} else {
+						$(this).css('border','0');
+					}
+				});		
+				$('#category').on('change', function() {
+					console.log($(this).attr('value'));
+					if($(this).attr('value') == '') {
+						$(this).css('border','1px solid #F00');
+					} else {
+						$(this).css('border','0');
+					}
+				});
+				$('#post-content').on('change', function() {
+					console.log($(this).text());
+					if($(this).text() == '') {
+						$(this).css('border','1px solid #F00');
+					} else {
+						$(this).css('border','0');
+					}
+				});
+				$('#add-submit').click(function(e){
+					e.preventDefault();
+					var form = $('#post-form');
+					
+					var title = $('#title').attr('value');
+					var category = $('#category').attr('value');
+					var text = $('#post-content').attr('value');
+					
+					var error = '';
+					
+					if(title !== '' &&
+						category !== '' &&
+						text !== ''
+					) {
+						form.submit();
+					} else {
+						if(title == '') {
+							error = error.concat("Title is empty.\n");
+							$('#title').css('border','1px solid #F00');
+						} else {
+							$('#title').css('border','0');
+						}
+						if(category == '') {
+							error = error.concat("Category is empty.\n");
+							$('#category').css('border','1px solid #F00');
+						} else {
+							$('#category').css('border','0');
+						}
+						if(text == '') {
+							error = error.concat("Content is empty.\n");
+							$('#post-content').css('border','1px solid #F00');
+						} else {
+							$('#post-content').css('border','0');
+						}
+					}
+				});
+			});
+		</script>
       </div>
     </div>
 </div>
