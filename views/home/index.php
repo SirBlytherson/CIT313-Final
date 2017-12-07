03afeb9a4170e834cdea86f15a802ea69ed77262
NextDorm@gmail.com <NextDorm@gmail.com>
2017-12-06 22:54:36 -0500

    Begin trying to add weather widget sidebar to home page

--- views/home/index.php
+++ views/home/index.php
@@ -1,6 +1,7 @@
 <?php include('views/elements/header.php');?>
 <div class="container">
-	<div class="page-header">
+<div class="row">
+	<div class="col-md-12">
 	<?php
     if((!isset($message) || $message == null) && isset($_SESSION['message'])) {
       $message = $_SESSION['message'];
@@ -13,17 +14,28 @@
 			<?php if(isset($message)) echo $message?>
 		</div>
 	  <?php }?>
-	<h1>Latest News from <?php echo $rss_title; ?></h1>
-  </div>
-  <?php
-    foreach ($rss_feed as $article) {
-      ?>
-      <div class="page-header">
-        <h4><a href="<?php echo $article->guid; ?>"><?php echo $article->title?></a><?php echo ' ('.date('F j, Y, g:i a',strtotime($article->pubDate)).')'; ?></h4>
-        <p><?php echo $article->description; ?></p>
-      </div>
-      <?php
-    }
-  ?>
+	</div>
+</div>
+
+<div class="row">
+	<h1 class="col-md-12">Latest News from <?php echo $rss_title; ?></h1>
+</div>
+  
+<div class="row">
+	<div class="col-md-9 col-lg-9 col-sm-3 col-xs-3">
+		<?php
+		foreach ($rss_feed as $article) {
+			if($article->title) {
+		?>
+		  <div>
+			<h4><a href="<?php echo $article->guid; ?>"><?php echo $article->title?></a><?php echo ' ('.date('F j, Y, g:i a',strtotime($article->pubDate)).')'; ?></h4>
+			<p><?php echo $article->description; ?></p><br>
+		  </div>
+		<?php
+			}
+		}
+		?>
+	</div>
+</div>
 </div>
 <?php include('views/elements/footer.php');?>
